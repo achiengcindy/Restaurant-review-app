6136a4f25375348a4dffebc4f85c2dc5f8f8b847
achiengcindy <achiengcindy36@gmail.com>
2018-08-12 22:31:30 +0300

    grid layout on main page

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY2luZHlhY2hpZW5nIiwiYSI6ImNqa3A3dXM0YTJ3MXozbHFoeGNrc2h0bjEifQ.4GQ5hejQNMDSAk48fvpoaw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
